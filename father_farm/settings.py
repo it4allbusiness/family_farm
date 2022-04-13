bb966dd05f3a343b7f2e9b208a79b4f0f028077e
it4allbusiness <102702830+it4allbusiness@users.noreply.github.com>
2022-04-12 20:46:06 -0400

    Update settings.py

--- father_farm/settings.py
+++ father_farm/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*ex=8bjep##f$w$da*f-aw_unda0n51byxs^i_7wl&lbkd#kp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
